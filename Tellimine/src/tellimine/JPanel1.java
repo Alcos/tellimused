6d7998e071382a39d8b7c88f339b04291ae43ba9
Alcos <Alcos@192.168.3.137>
2012-10-04 16:27:31 +0300

    lisatud teek

--- Tellimine/src/tellimine/JPanel1.java
+++ Tellimine/src/tellimine/JPanel1.java
@@ -2,20 +2,31 @@ package tellimine;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
 public class JPanel1 extends JPanel{
+   //private 
+    private JButton nupp;
     
-   public void panel1(){
-       
-       JPanel panel1 = new JPanel();
-       panel1.setSize(700,50);
-       panel1.setBorder(BorderFactory.createLineBorder(Color.black));
+    
+    
+  public JPanel1(){super();
+  initKomp();}
+    
+   
+   private void initKomp(){
+     this.setPreferredSize(new Dimension(700,50));
+     this.setLayout(new BorderLayout());
+       this.setBorder(BorderFactory.createLineBorder(Color.black));
        JLabel label1 = new JLabel("Tellimine");
-       add(panel1);
-       panel1.add(label1);
+       //add(panel1);
+       this.add(label1,BorderLayout.SOUTH);
+       this.setVisible(true);
+   
    }
 }
--- Tellimine/src/tellimine/Tellimine.java
+++ Tellimine/src/tellimine/Tellimine.java
@@ -1,27 +1,49 @@
 package tellimine;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 
 public class Tellimine  extends JFrame{
 
     public Tellimine(){
+          super("Tellimine");
+         
+    }
+    
+    
+    public  void looFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       this.setPreferredSize(new Dimension(800, 300));
+       this.setLayout(new BorderLayout());
+       
+        JPanel1 panel = new JPanel1();
+        //panel.panel1();
+        this.add(panel, BorderLayout.NORTH);
+        //lisa teine paneel
         
-       super("Tellimine");
-       super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       super.setSize(800, 300);
-       super.setLayout(new BorderLayout());
-       super.setVisible(true);
-        
+        this.setVisible(true);
+        this.pack();
+       
+    
     }
     
     public static void main(String[] args) throws Exception{
         
-        Tellimine frame = new Tellimine();
-        JPanel1 panel = new JPanel1();
-        panel.panel1();
-        frame.add(panel);
+        
+       
+       final Tellimine raam = new Tellimine();
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+        public void run() {
+                 raam.looFrame();
+                 
+                 }
+             });
+        
+        
         /*JPanel2 paneel = new JPanel2();
         paneel.panel2();
         frame.add(paneel);*/
